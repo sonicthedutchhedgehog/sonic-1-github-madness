54383f9ce160834cef0f4351550344ae8a8f7439
zdrmonster1 <37600325+guest123guest@users.noreply.github.com>
2021-08-10 18:12:04 -0500

    Way past cool!

--- sonic1.asm
+++ sonic1.asm
@@ -35,8 +35,8 @@ Vectors:	dc.l $FFFE00, EntryPoint, BusError, AddressError
 		dc.l ErrorTrap,	ErrorTrap, ErrorTrap, ErrorTrap
 Console:	dc.b 'SEGA MEGA DRIVE ' ; Hardware system ID
 Date:		dc.b '(C)SEGA 1991.APR' ; Release date
-Title_Local:	dc.b 'SONIC THE               HEDGEHOG                ' ; Domestic name
-Title_Int:	dc.b 'SONIC THE               HEDGEHOG                ' ; International name
+Title_Local:	dc.b 'WHEN AMY SEES THIS SHELL MELT                   ' ; Domestic name
+Title_Int:	dc.b 'GOTTA JUICE IT, THIS IS WAY PAST COOL!          ' ; International name
 Serial:		dc.b 'GM 00001009-00'   ; Serial/version number
 Checksum:	dc.w 0
 		dc.b 'J               ' ; I/O support
