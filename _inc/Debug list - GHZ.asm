caa46aa9c27d2d2a440614a4ce0738621b9151ba
zdrmonster1 <37600325+guest123guest@users.noreply.github.com>
2021-08-10 19:01:26 -0500

    Gee, it sure is boring around here.

--- _inc/Debug list - GHZ.asm
+++ _inc/Debug list - GHZ.asm
@@ -5,7 +5,7 @@
 	dc.l Map_obj25+$25000000	; mappings pointer, object type * 10^6
 	dc.b 0,	0, $27,	$B2		; subtype, frame, VRAM setting (2 bytes)
 	dc.l Map_obj26+$26000000
-	dc.b 0,	0, 6, $80
+	dc.b 7,	0, 6, $80
 	dc.l Map_obj1F+$1F000000
 	dc.b 0,	0, 4, 0
 	dc.l Map_obj22+$22000000
