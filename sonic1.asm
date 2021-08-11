fd892213c2e239254949fe49c6f6a4d2bed0cdb9
sonicthedutchhedgehog <69474032+sonicthedutchhedgehog@users.noreply.github.com>
2021-08-11 10:11:01 +0200

    explotions

--- sonic1.asm
+++ sonic1.asm
@@ -34800,6 +34800,19 @@ Hurt_Shield:
 		bset	#1,$22(a0)
 		move.w	#-$900,$12(a0)	; make Sonic bounce away from the object
 		move.w	#-$900,$10(a0)
+		move.b	#$3F,0(a1)	; load explosion object
+		move.w	8(a0),8(a1)
+		move.w	$C(a0),$C(a1)
+		jsr	(RandomNumber).l
+		move.w	d0,d1
+		moveq	#0,d1
+		move.b	d0,d1
+		lsr.b	#2,d1
+		subi.w	#$20,d1
+		add.w	d1,8(a1)
+		lsr.w	#8,d0
+		lsr.b	#3,d0
+		add.w	d0,$C(a1)
 		btst	#6,$22(a0)
 		beq.s	Hurt_Reverse
 		move.w	#-$200,$12(a0)
