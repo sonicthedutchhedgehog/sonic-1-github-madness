c4ac5617d855fc7e87a58832771f95b5147bb9b1
sonicthedutchhedgehog <69474032+sonicthedutchhedgehog@users.noreply.github.com>
2021-08-14 12:22:23 +0200

    50 RINGS (bc conteniuethtrhtjrf)

---
+++ _maps/obj81.asm
@@ -0,0 +1,17 @@
+		dc.w byte_50AE-Map_obj81, byte_50BE-Map_obj81
+		dc.w byte_50CE-Map_obj81
+byte_509E2:	dc.b 3
+		dc.b 4,	5, 0, $15, $FC	; Sonic	on floor
+		dc.b $F4, $A, 0, 6, $EC
+		dc.b $F4, 6, 0,	$F, 4
+byte_50AE2:	dc.b 3
+		dc.b 4,	5, 0, $19, $FC	; Sonic	on floor #2
+		dc.b $F4, $A, 0, 6, $EC
+		dc.b $F4, 6, 0,	$F, 4
+byte_50BE2:	dc.b 3
+		dc.b 4,	5, 0, $1D, $FC	; Sonic	on floor #3
+		dc.b $F4, $A, 0, 6, $EC
+		dc.b $F4, 6, 0,	$F, 4
+byte_50CE2:	dc.b 2
+		dc.b $60, 9, $20, 0, $E8 ; circle on the floor
+		dc.b $60, 9, $28, 0, 0
