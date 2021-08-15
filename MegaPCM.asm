afdc71162f7aabff37280491c8f8645e27817f40
DG <derpguy125@gmail.com>
2021-08-14 21:04:42 -0400

    one more time :D
    fweifjiwefj

--- MegaPCM.asm
+++ MegaPCM.asm
@@ -59,7 +59,7 @@ MegaPCM:
 	DAC_Entry	$0A, Snare, dpcm		; $82	- Snare
 	DAC_Entry	$1B, Timpani, dpcm		; $83	- Timpani
 	DAC_Entry	$03, whoosh, pcm		; $84	- Amogus Whoosh SFX
-	DAC_Entry	$03, crack, pcm			; $85	- Amogus Crack SFX
+	DAC_Entry	$02, crack, pcm			; $85	- Amogus Crack SFX
 	dc.l	0,0					; $86	- <Free>
 	dc.l	0,0					; $87	- <Free>
 	DAC_Entry	$12, Timpani, dpcm		; $88	- Hi-Timpani
